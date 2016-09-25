b5b93fdb88ab4ee51cf3f25f0b504c77a1e0df5d
Sam Mbale <smbale@gmail.com>
2016-09-25 12:19:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,140 @@
-<!DOCTYPE html>
-<html data-wf-site="5582f9e5792714e458bb85b8" data-wf-page="5582f9e5792714e458bb85b9">
+<!doctype html>
+<html>
 <head>
-	<meta charset="utf-8">
-	<title>Catalyze</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-	<link rel="stylesheet" type="text/css" href="css/normalize.css">
-	<link rel="stylesheet" type="text/css" href="css/webflow.css">
-	<link rel="stylesheet" type="text/css" href="css/simplify-test.webflow.css">
-	<script src="//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
-	<script>
-		WebFont.load({
-			google: {
-				families: ["Inconsolata:400,400italic,700,700italic"]
-			}
-		});
-	</script>
-	<script type="text/javascript" src="js/modernizr.js"></script>
-	<link rel="apple-touch-icon" href="//daks2k3a4ib2z.cloudfront.net/img/webclip.png">
-	<style>
-		body {
-			font-family: 'Avenir Next', Avenir, 'Helvetica Neue', Helvetica, Arial, sans-serif;
-			text-rendering: optimizeLegibility;
-		}
-		h1 {
-			font-family: 'Avenir Next', Avenir, 'Helvetica Neue', Helvetica, Arial, sans-serif;
-		}
-	</style>
+<meta charset="UTF-8">
+<meta name="description" content="A beautiful HTML & CSS built iPhone 6 mock for your website." />
+<title>Dashboard - Catlyze</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="custom.css">
+<link rel="shortcut icon" href="../favicon.png">
+
 </head>
+
 <body>
-<div class="w-container message">
-	<div>
-		<h1 class="main-message">catalyze</h1>
-	
-       </div>
-<div class="w-section footer-section">
-	<div class="logo-container"><img class="logo" src="images/simplifyLogo@2x.png" width="102">
+
+    <div class="dark half center-block light">
+        <div class="iphone-container center-block">
+            <div class="phone-shape small">
+                <span class="button-one buttons"></span>
+                <span class="button-two buttons"></span>
+                <span class="button-three buttons"></span>
+                <span class="button-four button last"></span>
+                <div class="top-details">
+                    <span class="camera"></span>
+                    <span class="circle"></span>
+                    <span class="speaker"></span>
+                </div>
+                <div class="phone-screen">
+                    <div class="navbar-default">
+                        <div class="navbar-title">CATALYZE</div>
+                        <a href="#"><img src="https://storage.googleapis.com/catalyse/Catalyst-Mobile/images/hamburger.png" class="hamburger-icon"></a>
+                        <a href="#"><div class="name"><div class="name-text">Sipho</div></div></a>
+                    </div>
+                    <div class="jumbotron">
+                        <div class="overlay">
+                            <div class="jumbotron-title">INVEST IN LOCAL BUSINESSES AND</div>
+                            <div class="jumbotron-title2">GROW AS A COMMUNITY</div>
+                        </div>
+                    </div>
+                    <div class="dashboard-details-container">
+                        <div class="dashboard-title">Investment opportunities</div>
+                        <div class="dashboard-filter">Filter</div>
+                    </div>
+                    <div class="row">
+                        <div class="opportunities-container">
+                            <div class="row">
+                                <div class="project-name"></div>
+                                <img src="https://storage.googleapis.com/catalyse/Catalyst-Mobile/images/demo.jpg" class="opportunities-images">
+                            </div>
+                            <div class="amount-raised">$3,360 was invested by 6 people</div>
+                            <div class="amount-raised-container center-block">
+                                <div class="amount-raised-container2"></div>
+                            </div>
+                            <div class="amount-raised2">of $10,000</div>
+                            <div class="amount-raised3">28 Days Left</div>
+                        </div>
+                        <div class="opportunities-container">
+                            <div class="row">
+                                <div class="project-name"></div>
+                                <img src="https://storage.googleapis.com/catalyse/Catalyst-Mobile/images/demo.jpg" class="opportunities-images">
+                            </div>
+                            <div class="amount-raised">$3,360 was invested by 6 people</div>
+                            <div class="amount-raised-container center-block">
+                                <div class="amount-raised-container2"></div>
+                            </div>
+                            <div class="amount-raised2">of $10,000</div>
+                            <div class="amount-raised3">28 Days Left</div>
+                        </div>
+                        <div class="opportunities-container">
+                            <div class="row">
+                                <div class="project-name"></div>
+                                <img src="https://storage.googleapis.com/catalyse/Catalyst-Mobile/images/demo.jpg" class="opportunities-images">
+                            </div>
+                            <div class="amount-raised">$3,360 was invested by 6 people</div>
+                            <div class="amount-raised-container center-block">
+                                <div class="amount-raised-container2"></div>
+                            </div>
+                            <div class="amount-raised2">of $10,000</div>
+                            <div class="amount-raised3">28 Days Left</div>
+                        </div>
+                        <div class="opportunities-container">
+                            <div class="row">
+                                <div class="project-name"></div>
+                                <img src="https://storage.googleapis.com/catalyse/Catalyst-Mobile/images/demo.jpg" class="opportunities-images">
+                            </div>
+                            <div class="amount-raised">$3,360 was invested by 6 people</div>
+                            <div class="amount-raised-container center-block">
+                                <div class="amount-raised-container2"></div>
+                            </div>
+                            <div class="amount-raised2">of $10,000</div>
+                            <div class="amount-raised3">28 Days Left</div>
+                        </div>
+                        <div class="opportunities-container">
+                            <div class="row">
+                                <div class="project-name"></div>
+                                <img src="https://storage.googleapis.com/catalyse/Catalyst-Mobile/images/demo.jpg" class="opportunities-images">
+                            </div>
+                            <div class="amount-raised">$3,360 was invested by 6 people</div>
+                            <div class="amount-raised-container center-block">
+                                <div class="amount-raised-container2"></div>
+                            </div>
+                            <div class="amount-raised2">of $10,000</div>
+                            <div class="amount-raised3">28 Days Left</div>
+                        </div>
+                        <div class="opportunities-container">
+                            <div class="row">
+                                <div class="project-name"></div>
+                                <img src="https://storage.googleapis.com/catalyse/Catalyst-Mobile/images/demo.jpg" class="opportunities-images">
+                            </div>
+                            <div class="amount-raised">$3,360 was invested by 6 people</div>
+                            <div class="amount-raised-container center-block">
+                                <div class="amount-raised-container2"></div>
+                            </div>
+                            <div class="amount-raised2">of $10,000</div>
+                            <div class="amount-raised3">28 Days Left</div>
+                        </div>
+                        <div class="opportunities-container">
+                            <div class="row">
+                                <div class="project-name"></div>
+                                <img src="https://storage.googleapis.com/catalyse/Catalyst-Mobile/images/demo.jpg" class="opportunities-images">
+                            </div>
+                            <div class="amount-raised">$3,360 was invested by 6 people</div>
+                            <div class="amount-raised-container center-block">
+                                <div class="amount-raised-container2"></div>
+                            </div>
+                            <div class="amount-raised2">of $10,000</div>
+                            <div class="amount-raised3">28 Days Left</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="circle-button"></div>
+            </div>
+        </div>
 	</div>
-</div>
+
+        
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript" src="scripts.js"></script>
 </body>
 </html>
+Contact GitHub API Training Shop Blog About
